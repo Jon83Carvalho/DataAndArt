c1f8a5a9642491d3c297e642c1c5ee6bfd266a9c
Jon83Carvalho <jonas.carvalho@gmail.com>
2022-08-14 17:37:53 -0300

    Manyupdates

--- datarthome/Art3.js
+++ datarthome/Art3.js
@@ -1,46 +1,90 @@
-import { scrollView, Text,ImageBackground, StyleSheet} from 'react-native';
-import React from 'react'; 
-import {range} from 'd3';
+import {Viz2} from './Viz2'
+import React, {useState,useRef} from 'react'
+import {csvParse} from 'd3';
+import { View, Text } from 'react-native';
+import {useData_stock} from './useData_stock'
+import {
+  useFonts,
+  YanoneKaffeesatz_200ExtraLight,
+  YanoneKaffeesatz_300Light,
+  YanoneKaffeesatz_400Regular,
+  YanoneKaffeesatz_500Medium,
+  YanoneKaffeesatz_600SemiBold,
+  YanoneKaffeesatz_700Bold,
+} from '@expo-google-fonts/yanone-kaffeesatz';
 
 
-import {Viz2} from './Viz2';
-
-const image={uri:"https://github.com/Jon83Carvalho/DataAndArt/blob/main/eye.jpg?raw=true"};
-
-const circleRadius=30;
-
-const array=range(1);
+localStorage.removeItem('pdata');
+localStorage.removeItem('firstdisplay');
 
 
+import useSWR from 'swr';
+
+import { laggy } from './laggy';
+import axios from 'axios';
 const margin={
   top:30,
   right:50,
-  bottom:50,
+  bottom:80,
   left:50
 };
-const xAxislabelOffset=70;
-const yAxislabelOffset=50;
-
-
-
 
 export function Art3() {
-    return (
-   
-  <Viz2
-    width="100vw"
-    height="100vh"
-    marginTop={margin.top}
-    marginRight={margin.right}
-    marginBottom={margin.bottom}
-    marginLeft={margin.left}
-    xAxislabelOffset={xAxislabelOffset}
-    yAxislabelOffset={yAxislabelOffset}
     
-  />
+  const [start, setStart] = useState(false);
  
-
- 
-    )
+    
+    let [fontsLoaded] = useFonts({
+      YanoneKaffeesatz_200ExtraLight,
+      YanoneKaffeesatz_300Light,
+      YanoneKaffeesatz_400Regular,
+      YanoneKaffeesatz_500Medium,
+      YanoneKaffeesatz_600SemiBold,
+      YanoneKaffeesatz_700Bold,
+    })
+  
+    
+    
+  
+    
+    const svgRef = useRef();
+    
+    const {data,data2}=useData_stock();
+      
+  
+  
+  
+    
+  if (!data||!data2 || !fontsLoaded) 
+  {
+    return <div>
+            <Text>Carregando...</Text>
+           </div>
   }
- 
+  
+
+  
+  
+   
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <>
+       <button onClick={() => setStart(!start)}>
+        {start ? "Before the War" : "The War!"}
+      </button>
+      <Viz2
+      data={data}
+      data2={data2} 
+      svgRef={svgRef}
+      marginTop={margin.top}
+      marginRight={margin.right}
+      marginBottom={margin.bottom}
+      marginLeft={margin.left}
+ />
+        
+      </>
+      
+      </View>
+      
+    );
+  }
--- datarthome/StockMarks.js
+++ datarthome/StockMarks.js
@@ -1,5 +1,5 @@
-import React,{memo,useState,useCallback} from 'react';
-import {format} from 'd3';
+import React,{memo,useState,useCallback, useEffect,useRef} from 'react';
+import {select,format} from 'd3';
 
 
 const f = format(".1f");
@@ -30,39 +30,11 @@ export const StockMarks=({
   larr,
   rcircle,
   countryLegend,
-  rfactor
+  rfactor,
+  svgRef
 })=>{
-	const tempo = new Date();
-  let seconds = tempo.getSeconds();	
-  const maxc=Math.max.apply(Math, data.map(function(d) { return d.price; }))
-  const ming=Math.min.apply(Math, data.map(function(d) { return d.price; }))
-
 
   
+  return (<></>)
   
-  return (
-        data.map(function(d,i){
-             
-          				return (
-              <>
-                	<circle 
-                   className="mark"
-                   cx={isNaN(xScale(Math.log10(xValue(d))))?0:xScale(Math.log10(xValue(d)))} 
-                   cy={yScale(yValue(d))} 
-                   r={isNaN(rScale(Math.log10(rValue(d))))?0:rScale(Math.log10(rValue(d)))*0.02}
-                   opacity="0.8"
-                   fill={colorScale(colorValue(d))}
-                     
-                  >
-                   
-                </circle>
-
-             
-
-               
-              </>
-                      );
-                 })
-          )
-
 }
