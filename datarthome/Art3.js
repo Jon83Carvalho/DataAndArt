347293b366059d7c281d733e0a14daceeaef3daa
Jon83Carvalho <jonas.carvalho@gmail.com>
2022-08-21 23:10:15 -0300

    updates

--- datarthome/Art3.js
+++ datarthome/Art3.js
@@ -1,8 +1,10 @@
 import {Viz2} from './Viz2'
 import React, {useState,useRef} from 'react'
-import {csvParse} from 'd3';
-import { View, Text } from 'react-native';
+import {select} from 'd3';
+import { View, Text,Button, TouchableOpacity } from 'react-native';
 import {useData_stock} from './useData_stock'
+
+
 import {
   useFonts,
   YanoneKaffeesatz_200ExtraLight,
@@ -22,12 +24,23 @@ import useSWR from 'swr';
 
 import { laggy } from './laggy';
 import axios from 'axios';
+
+const screenwidth=+select("#root").style("width").slice(0,-2)
+const screenheight=+select("#root").style("height").slice(0,-2)
+
+const size_unit_w=screenwidth/20
+const size_unit_h=screenheight/20
+
+const aspect_ratio=5;
+
+
 const margin={
-  top:30,
-  right:50,
-  bottom:80,
-  left:50
+  top:size_unit_h,
+  right:size_unit_w*7.4,
+  bottom:size_unit_h*3,
+  left:size_unit_w
 };
+
 
 export function Art3() {
     
@@ -54,11 +67,9 @@ export function Art3() {
     
     const [dataUsed,setdataUsed]=useState([{data1:"data"}])
 
-    
+  
 
-  
-  
-  
+ 
     
   if (!data||!data2 || !fontsLoaded) 
   {
@@ -71,17 +82,12 @@ export function Art3() {
   
    
     return (
+      <React.Fragment>
+      
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       <React.Fragment>
-        <button onClick={() => {
+       
         
-        setStart(!start)
-        setdataUsed(data2)
-
-      }
-        }>
-        {start ? "Before the War!" : "The War!"}
-      </button>
+      
       <Viz2
       data={start ? dataUsed : data}
       data2={data2} 
@@ -92,9 +98,51 @@ export function Art3() {
       marginLeft={margin.left}
  />
         
-        </React.Fragment>
+       
       
       </View>
+      <View 
+      style={{
+        position:'absolute',
+        top:(screenheight-size_unit_h*10/aspect_ratio)/2,
+        right:(screenwidth+size_unit_h*10)/2,
+        width:size_unit_h,
+        height:size_unit_h/aspect_ratio,
+        opacity:"70%"
+
+        }}
+      >
+        <TouchableOpacity
+        style={{
+        backgroundColor:start ? "#880000" : "#006600",
+        activeOpacity:0.80,
+        width:size_unit_h*10,
+        height:size_unit_h*10/aspect_ratio
+      }}
+      onPress={() => {
+        
+        setStart(!start)
+        setdataUsed(data2)
+
+        }
+    }  
+        >
+          <Text style={{
+            textAlign:'center',
+            color:'white',
+            alignItems: 'center',
+            fontWeight: 'bold',
+            fontSize: "1.3rem",
+            marginTop: size_unit_h*10/aspect_ratio/2.5,
+            fontFamily:'YanoneKaffeesatz_400Regular'
+          }}>
+          {start ? "The War!" : "Before the War!"}
+          </Text>
+        
+        </TouchableOpacity>
+        
+      </View>
+      </React.Fragment>
       
     );
   }
